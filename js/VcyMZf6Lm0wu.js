19cc947d401bb3a69e191ea4e56db5e91adba10b
MeFuzi <97913207+MeFuzi@users.noreply.github.com>
2022-11-06 19:43:35 +0300

    Add files via upload

---
+++ js/VcyMZf6Lm0wu.js
@@ -0,0 +1,1 @@
+tippy("#statusContent",{content:"Discord durumu"}),tippy("#city",{content:"Lokasyon"}),tippy("#discord",{content:"Discord Sunucum"}),tippy("#github",{content:"Github Hesabım"}),tippy("#mail",{content:"Bana Mail Gönder"});
